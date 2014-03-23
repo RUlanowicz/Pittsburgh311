2536d783d62d8648307b2a5ab7c0f82b595b3c7d
Longhao Li <longhaoli@LonghaodeMacBook-Pro.local>
2014-03-22 23:52:42 -0400

    Comments page done

--- pittsburgh311/src/main/java/edu/pitt/cs1635/pittsburgh311/Comments.java
+++ pittsburgh311/src/main/java/edu/pitt/cs1635/pittsburgh311/Comments.java
@@ -1,6 +1,8 @@
 package edu.pitt.cs1635.pittsburgh311;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,9 +10,15 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Comments extends ActionBarActivity {
 
+    static final int REQUEST_TAKE_PHOTO = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +26,23 @@ public class Comments extends ActionBarActivity {
 
         Log.i("Comments", "In here");
 
-        final int REQUEST_IMAGE_CAPTURE = 1;
-
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
@@ -45,6 +65,25 @@ public class Comments extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 
 }
