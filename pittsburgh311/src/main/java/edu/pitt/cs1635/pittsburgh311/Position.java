21c99782da0f916a90686d0da028a8f6fa92759e
Longhao Li <longhaoli@LonghaodeMacBook-Pro.local>
2014-04-22 17:03:58 -0400

    position GPS

--- pittsburgh311/src/main/java/edu/pitt/cs1635/pittsburgh311/Position.java
+++ pittsburgh311/src/main/java/edu/pitt/cs1635/pittsburgh311/Position.java
@@ -3,6 +3,9 @@ package edu.pitt.cs1635.pittsburgh311;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -37,10 +40,11 @@ import edu.pitt.cs1635.pittsburgh311.model.ProfileManager;
 /**
  * Created by ulanowicz on 3/26/14.
  */
-public class Position extends FragmentActivity {
+public class Position extends FragmentActivity implements LocationListener, LocationSource{
     String city = null;
     Incident myIncident = Incident.getInstance();
     ProfileManager userProfile;
+    private LocationManager locationManager;
 
     public static String getContent(HttpResponse response) throws IOException {
         BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
@@ -67,8 +71,76 @@ public class Position extends FragmentActivity {
         Toast.makeText(getApplicationContext(),"Press and Hold Pin to Move",Toast.LENGTH_LONG).show();
 
         LatLng pgh = new LatLng(40.441814, -80.012794);
-
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(pgh, 13));
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if(locationManager != null)
+        {
+            boolean gpsIsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean networkIsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if(gpsIsEnabled)
+            {
+                Log.d("GPS Enabled", "GPS Enabled");
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 10F, this);
+                    Location location = locationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        pgh = new LatLng(location.getLatitude(), location.getLongitude());
+                    }
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(pgh, 16));
+                myIncident.setLat(Double.toString(pgh.latitude));
+                myIncident.setLon(Double.toString(pgh.longitude));
+                List<Address> addresses = null;
+                try {
+                    addresses = geocoder.getFromLocation(pgh.latitude, pgh.longitude, 1);
+                    myIncident.setAddress(addresses.get(0).getAddressLine(0));
+                    city = addresses.get(0).getAddressLine(1);
+                    if(!city.contains("Pittsburgh")){
+                        submit.setEnabled(false);
+                    }
+                    else{
+                        submit.setEnabled(true);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                if (networkIsEnabled) {
+                    Log.d("Network Position Enabled", "Network Position");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000L, 10F, this);
+                    Location location = locationManager
+                            .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location != null) {
+                        pgh = new LatLng(location.getLatitude(), location.getLongitude());
+                    }
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(pgh, 15));
+                    myIncident.setLat(Double.toString(pgh.latitude));
+                    myIncident.setLon(Double.toString(pgh.longitude));
+                    List<Address> addresses = null;
+                    try {
+                        addresses = geocoder.getFromLocation(pgh.latitude, pgh.longitude, 1);
+                        myIncident.setAddress(addresses.get(0).getAddressLine(0));
+                        city = addresses.get(0).getAddressLine(1);
+                        if(!city.contains("Pittsburgh")){
+                            submit.setEnabled(false);
+                        }
+                        else{
+                            submit.setEnabled(true);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        }
+        else
+        {
+
+        }
+
 
         Marker issueLoc = map.addMarker(new MarkerOptions()
                 .title("Issue Location")
@@ -117,6 +189,37 @@ public class Position extends FragmentActivity {
             }
         });
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+
+    }
+
+    @Override
+    public void deactivate() {
+
+    }
+
     class uploadReport extends AsyncTask<Void,Void,String> {
 
         @Override
